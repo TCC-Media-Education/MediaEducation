1950829669fefcd18f2293145496291320bbaaa9
igorcardonaalves <igorcardonaalves@hotmail.com>
2022-10-02 22:33:29 +0000

    Colocando texto em tutorial, créditos e enredo

--- tcc_project/src/Pages/Tutorial/Tutorial.jsx
+++ tcc_project/src/Pages/Tutorial/Tutorial.jsx
@@ -5,6 +5,9 @@ function Tutorial() {
     return (
         <div>
             <h1>Tutorial</h1>
+            <h2>Analise e classifique as seguintes notícias. Se ela for verdadeira, clique em "Verdadeiro". Caso contrário, clique em "falso".
+                No começo de cada rodada, você pode rever no máximo 2 vezes as informações por fase as informações necessárias para receber ajuda.
+                Você também pode conferir a data e a fonte da notícia. </h2>
         </div>
     )
 }
